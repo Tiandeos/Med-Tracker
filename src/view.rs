cb3f974a3aa3be9be5f0d536724b8b2d99a49bce
Tiandeos <46683526+Tiandeos@users.noreply.github.com>
2025-09-08 15:50:15 +0300

    Added side navbar and main content

--- src/view.rs
+++ src/view.rs
@@ -1,20 +1,34 @@
 
 use iced::{self as ice, Element, Length, Length::Fill};
-use ice::widget::{button,text,row,container};
+use ice::widget::{button,text,row,column,container};
 
-use crate::states::message::Message;
+use crate::states::{message::Message, panel::Panel};
 
 pub fn view(a : &String) -> Element<Message> {
-    container(row![
-        text(a).size(15).width(Length::FillPortion(1)),
-        button("aaa").on_press(Message::OpenPanel).width(Length::FillPortion(2)).padding(100),
-    ]
-    .spacing(10)
-    )
-    .padding(20)
-    .center_x(Fill)
-    .center_y(Fill)
+    let sidebar = container( // Side navigation Bar
+    column![
+        button("panel 1").on_press(Message::OpenPanel(Panel::Time)).
+        height(Length::FillPortion(1)).width(Fill),
+        button("panel 2").on_press(Message::OpenPanel(Panel::Record))
+        .height(Length::FillPortion(1)).width(Fill),
+        button("panel 3").on_press(Message::OpenPanel(Panel::Stock))
+        .height(Length::FillPortion(1)).width(Fill),
+        button("panel 4").on_press(Message::OpenPanel(Panel::Settings))
+        .height(Length::FillPortion(1)).width(Fill)
+    ])
+    .width(Length::FillPortion(1)) // Sidebar takes 1/4 of the width
+    .height(Fill);
+
+    let main_content = container(
+        column![
+            
+        ]
+    ).width(Length::FillPortion(3)) // Main content takes 3/4 of the width
+    .height(Fill);
+    row![
+        sidebar,
+        main_content
+    ].width(Fill)
+    .height(Fill)
     .into()
-    
-    
 }
