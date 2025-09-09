3a13059e27cd63d314548736ea710cd3f57225e9
Tiandeos <46683526+Tiandeos@users.noreply.github.com>
2025-09-09 18:48:02 +0300

    Added icons to navbar buttons

--- src/ui/view.rs
+++ src/ui/view.rs
@@ -1,20 +1,41 @@
-use iced::{self as ice, alignment, Border, Element, Length, Length::Fill, Theme};
-use ice::widget::{button,text,row,column,container};
+use iced::{self as ice, alignment, Alignment, Border, Element, Length, Length::Fill, Renderer, Theme};
+use ice::ContentFit;
+use ice::widget::{Image,button,text,row,column,container,};
 use crate::states::{message::Message, panel::Panel};
 
+macro_rules! button_with_icon {
+($label:expr, $icon_path:expr) => {
+        container(
+            row![
+                Image::new($icon_path)
+                    .content_fit(ContentFit::Cover)
+                    .width(40)
+                    .height(40),
+                text($label)
+            ].spacing(10)
+        ).align_y(alignment::Vertical::Center)
+    };
+}
 pub fn view(a : &String) -> Element<Message> {
+    /*let button1 = container( row![
+        Image::new("icons/home.png")
+        .content_fit(ContentFit::Cover)
+        .width(40)
+        .height(40),
+        text("Today")
+    ].spacing(10));*/
     let sidebar = container( // Side navigation Bar
     column![
-        button(text("panel 1").align_y(alignment::Vertical::Center))
+        button(button_with_icon!("Home","icons/home.png"))
         .on_press(Message::OpenPanel(Panel::Time)).
         height(Length::FillPortion(1)).width(Fill),
-        button(text("panel 2").align_y(alignment::Vertical::Center))
+        button(button_with_icon!("Calendar","icons/calendar.png"))
         .on_press(Message::OpenPanel(Panel::Record))
         .height(Length::FillPortion(1)).width(Fill),
-        button(text("panel 3").align_y(alignment::Vertical::Center))
+        button(button_with_icon!("Medications","icons/pill.png"))
         .on_press(Message::OpenPanel(Panel::ManageMeds))
         .height(Length::FillPortion(1)).width(Fill),
-        button(text("panel 4").align_y(alignment::Vertical::Center))
+        button(button_with_icon!("Settings","icons/settings.png"))
         .on_press(Message::OpenPanel(Panel::Settings))
         .height(Length::FillPortion(1)).width(Fill)
     ])
