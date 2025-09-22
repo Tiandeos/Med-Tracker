ac8b4f6b3c5b47d2f41eaebaeef7395ad03c2e55
Tiandeos <46683526+Tiandeos@users.noreply.github.com>
2025-09-22 14:58:30 +0300

    Added dark light detection

--- src/ui/view.rs
+++ src/ui/view.rs
@@ -4,6 +4,7 @@ use crate::states::message::Message;
 use crate::states::state::State;
 use crate::ui::content::main_content;
 use crate::ui::sidebar::{side_bar, sidebar_border};
+use dark_light::{Error, Mode};
 use ice::widget::row;
 
 pub fn view(state: &State) -> Element<Message> {
@@ -16,6 +17,16 @@ pub fn theme(state: &State) -> Theme {
     if state.settings.is_theme_changed {
         state.settings.theme.clone()
     } else {
-        Theme::Nord
+        detect_dark_light().unwrap_or(Theme::CatppuccinMocha)
     }
+}
+fn detect_dark_light() -> Result<Theme, dark_light::Error> {
+    let mode = dark_light::detect()?;
+    let theme: Theme;
+    match mode {
+        Mode::Dark => theme = Theme::Nord,
+        Mode::Light => theme = Theme::Light,
+        Mode::Unspecified => theme = Theme::TokyoNightLight,
+    }
+    Ok(theme)
 }
