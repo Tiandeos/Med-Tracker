bedff1093b3d5194362c7e608c38ad95b7df8a85
Tiandeos <46683526+Tiandeos@users.noreply.github.com>
2025-10-02 14:28:50 +0300

    Added empty_new and check empty for record type

--- src/states/medication/record.rs
+++ src/states/medication/record.rs
@@ -5,12 +5,23 @@ pub struct Record {
     pub is_skipped: bool,      // Checks if medication is skipped or not
 }
 impl Record {
-    pub fn new(medication_index: usize, schedule_index: usize, time: [u8; 5]) -> Self {
+    pub fn new(schedule_index: usize, time: [u8; 5]) -> Self {
         Record {
             schedule_index,
             time,
             is_taken: false,
             is_skipped: false,
         }
+    }
+    pub fn empty_new() -> Self {
+        Record {
+            schedule_index: 0,
+            time: [0; 5],
+            is_taken: false,
+            is_skipped: false,
+        }
+    }
+    fn check_empty(&self) -> bool {
+        self.time == [0; 5]
     }
 }
