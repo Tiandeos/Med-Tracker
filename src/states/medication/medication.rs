b1e17657caceb9cce7b756d6932d082beba9ed69
Tiandeos <46683526+Tiandeos@users.noreply.github.com>
2025-10-22 14:30:11 +0300

    Added ids for medications, added uuid cargo

--- src/states/medication/medication.rs
+++ src/states/medication/medication.rs
@@ -1,14 +1,16 @@
 use crate::states::medication::schedule::Schedule;
 
 pub struct Medication {
-    pub name: String,            // Medication name
-    pub stock: u32,              // Medication stock
+    pub id: String, // Unique ID of medication
+    pub name: String,
+    pub stock: u32,
     pub schedule: Vec<Schedule>, // List of schedules of medication
 }
 impl Medication {
     pub fn new(name: String, stock: u32) -> Self {
         Medication {
             name,
+            id: uuid::Uuid::new_v4().to_string(),
             stock,
             schedule: Vec::new(),
         }
--- src/states/medication/mod.rs
+++ src/states/medication/mod.rs
@@ -1,4 +1,5 @@
 pub mod medication;
+pub mod occurrencestatus;
 pub mod periodtype;
 pub mod record;
 pub mod schedule;
