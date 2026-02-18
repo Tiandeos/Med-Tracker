57c6b310e11a0c20b763093f43b99a1918685d08
Tiandeos <46683526+Tiandeos@users.noreply.github.com>
2026-02-18 23:46:47 +0300

    Added shadow for schedule_container changed color

--- src/ui/style/time/container.rs
+++ src/ui/style/time/container.rs
@@ -1,11 +1,11 @@
 use ice::widget::container::Style;
 use ice::{Background, Color, Theme};
-use iced::{self as ice, Border};
+use iced::{self as ice, Border, Shadow, Vector};
 
 pub fn schedule_container(theme: &Theme) -> Style {
     let palette = theme.extended_palette();
     Style {
-        background: Some(Background::Color(palette.background.strong.color)),
+        background: Some(Background::Color(palette.background.weak.color)),
         border: Border {
             color: Color::BLACK,
             width: 1.0,
@@ -16,6 +16,11 @@ pub fn schedule_container(theme: &Theme) -> Style {
                 bottom_right: 45.0,
             },
             ..Default::default()
+        },
+        shadow: Shadow {
+            color: Color::BLACK,
+            offset: Vector { x: 0.01, y: 4.0 },
+            blur_radius: 4.0,
         },
         ..Default::default()
     }
